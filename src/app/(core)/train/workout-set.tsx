e49dd26740d180bbad708808303a0c22b15b3c63
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-09-25 10:36:35 +0200

    feat: debounce mutations to workoutSets

--- src/app/(core)/train/workout-set.tsx
+++ src/app/(core)/train/workout-set.tsx
@@ -1,9 +1,20 @@
 import { useUpdateWorkoutSet } from '@/api/workouts/queries';
-import { type TWorkoutSet } from '@/api/workouts/types';
+import { TWorkoutSetDto, type TWorkoutSet } from '@/api/workouts/types';
 import { Input } from '@/components/ui/input';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function WorkoutSet({ workoutSet, workoutId, setNumber }: { workoutSet: TWorkoutSet; workoutId: number; setNumber: number }) {
   const { mutate: updateWorkoutSet } = useUpdateWorkoutSet();
+  const debouncedUpdateWorkoutSet = useDebouncedCallback(
+    (payload: TWorkoutSetDto) =>
+      updateWorkoutSet({
+        workoutId,
+        workoutExerciseId: workoutSet.workoutExerciseId,
+        setId: workoutSet.id,
+        data: payload,
+      }),
+    500
+  );
 
   return (
     <li className="grid grid-cols-3 items-center">
@@ -12,27 +23,13 @@ export default function WorkoutSet({ workoutSet, workoutId, setNumber }: { worko
         type="number"
         placeholder="weight"
         defaultValue={workoutSet.weight?.toString() || ''}
-        onChange={e => {
-          updateWorkoutSet({
-            workoutId: workoutId,
-            workoutExerciseId: workoutSet.workoutExerciseId,
-            setId: workoutSet.id,
-            data: { weight: +e.target.value },
-          });
-        }}
+        onChange={e => debouncedUpdateWorkoutSet({ weight: parseInt(e.target.value) })}
       />
       <Input
         type="number"
         placeholder="reps"
         defaultValue={workoutSet.reps?.toString() || ''}
-        onChange={e => {
-          updateWorkoutSet({
-            workoutId: workoutId,
-            workoutExerciseId: workoutSet.workoutExerciseId,
-            setId: workoutSet.id,
-            data: { reps: +e.target.value },
-          });
-        }}
+        onChange={e => debouncedUpdateWorkoutSet({ reps: parseInt(e.target.value) })}
       />
     </li>
   );
