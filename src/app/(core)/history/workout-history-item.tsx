c5f032a1d89ef9c94d1778d275585cb61de014ec
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-09-27 17:14:53 +0200

    style: implement table for workout-history-item

--- src/app/(core)/history/workout-history-item.tsx
+++ src/app/(core)/history/workout-history-item.tsx
@@ -2,6 +2,7 @@ import { useDeleteWorkout } from '@/api/workouts/queries';
 import { type TWorkout } from '@/api/workouts/types';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { formatDate } from 'date-fns';
 
 export default function WorkoutHistoryItem({ data: { id, title, createdAt, workoutExercises } }: { data: TWorkout }) {
@@ -10,15 +11,35 @@ export default function WorkoutHistoryItem({ data: { id, title, createdAt, worko
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <div className="flex justify-between items-center">
+          <CardTitle>{title}</CardTitle>
+          <Button variant="ghost">...</Button>
+        </div>
         <CardDescription>{formatDate(createdAt, 'PP')}</CardDescription>
       </CardHeader>
       <CardContent>
-        <ul>
-          {workoutExercises.map(workoutExercise => (
-            <li key={workoutExercise.id}>{workoutExercise.exercise.name}</li>
-          ))}
-        </ul>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Exercise</TableHead>
+              <TableHead className="w-10 text-center">Sets</TableHead>
+              <TableHead className="w-30 text-end">Best Set</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {workoutExercises.map(workoutExercise => {
+              const bestSet = workoutExercise.workoutSets.length ? `${workoutExercise.workoutSets[0].weight} kg x ${workoutExercise.workoutSets[0].reps}` : '-';
+
+              return (
+                <TableRow key={workoutExercise.id}>
+                  <TableCell>{workoutExercise.exercise.name}</TableCell>
+                  <TableCell className="text-center">{workoutExercise.workoutSets.length}</TableCell>
+                  <TableCell className="text-end">{bestSet}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
         <Button onClick={() => deleteWorkout(id)}>DELETE</Button>
       </CardContent>
     </Card>
