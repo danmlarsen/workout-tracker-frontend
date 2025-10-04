4ff76961153b536bed618942852019673ef6bebf
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-10-04 13:47:22 +0200

    feat: add derrived workout duration and total weight lifted

--- src/app/(core)/workouts/workout-history-item.tsx
+++ src/app/(core)/workouts/workout-history-item.tsx
@@ -1,23 +1,71 @@
-import { type TWorkout } from '@/api/workouts/types';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { formatDate } from 'date-fns';
-import WorkoutHistoryItemDropdownMenu from './workout-history-item-dropdown-menu';
-import { useState } from 'react';
-import WorkoutModal from './workout-modal';
-import { Button } from '@/components/ui/button';
+import { type TWorkout } from "@/api/workouts/types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  addSeconds,
+  differenceInSeconds,
+  format,
+  formatDate,
+  startOfDay,
+} from "date-fns";
+import WorkoutHistoryItemDropdownMenu from "./workout-history-item-dropdown-menu";
+import { useState } from "react";
+import WorkoutModal from "./workout-modal";
+import { Button } from "@/components/ui/button";
+import { ClockIcon, WeightIcon } from "lucide-react";
 
 export default function WorkoutHistoryItem({ workout }: { workout: TWorkout }) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const { id, title, createdAt, workoutExercises } = workout;
+  const { id, title, createdAt, workoutExercises, completedAt } = workout;
+
+  const workoutDuration = completedAt
+    ? format(
+        addSeconds(
+          startOfDay(new Date()),
+          differenceInSeconds(new Date(completedAt), new Date(createdAt)),
+        ),
+        "HH:mm:ss",
+      )
+    : null;
+
+  const totalWeight = workout.workoutExercises.reduce(
+    (total, exercise) =>
+      total +
+      exercise.workoutSets.reduce(
+        (setTotal, curSet) =>
+          setTotal +
+          (!!curSet.completedAt
+            ? (curSet.weight ?? 0) * (curSet.reps ?? 0)
+            : 0),
+        0,
+      ),
+    0,
+  );
 
   return (
     <>
-      <WorkoutModal workout={workout} isOpen={modalIsOpen} onOpenChange={newState => setModalIsOpen(newState)} />
+      <WorkoutModal
+        workout={workout}
+        isOpen={modalIsOpen}
+        onOpenChange={(newState) => setModalIsOpen(newState)}
+      />
       <Card>
         <CardHeader>
-          <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between">
             <CardTitle>{title}</CardTitle>
             <div className="flex items-center gap-2">
               <Button variant="ghost" onClick={() => setModalIsOpen(true)}>
@@ -26,9 +74,21 @@ export default function WorkoutHistoryItem({ workout }: { workout: TWorkout }) {
               <WorkoutHistoryItemDropdownMenu workoutId={id} />
             </div>
           </div>
-          <CardDescription>{formatDate(createdAt, 'PP')}</CardDescription>
+          <CardDescription>{formatDate(createdAt, "EEEE PP")}</CardDescription>
         </CardHeader>
         <CardContent>
+          <div className="flex items-center gap-4">
+            {workoutDuration && (
+              <div className="flex items-center gap-2">
+                <ClockIcon className="size-4" /> <span>{workoutDuration}</span>
+              </div>
+            )}
+            {totalWeight && (
+              <div className="flex items-center gap-2">
+                <WeightIcon className="size-4" /> <span>{totalWeight}kg</span>
+              </div>
+            )}
+          </div>
           <Table>
             <TableHeader>
               <TableRow>
@@ -38,17 +98,24 @@ export default function WorkoutHistoryItem({ workout }: { workout: TWorkout }) {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {workoutExercises.map(workoutExercise => {
-                const completedSets = workoutExercise.workoutSets.filter(set => !!set.completedAt);
+              {workoutExercises.map((workoutExercise) => {
+                const completedSets = workoutExercise.workoutSets.filter(
+                  (set) => !!set.completedAt,
+                );
 
                 if (!completedSets) return null;
 
-                const bestSet = completedSets.length > 0 ? `${completedSets[0].weight} kg x ${completedSets[0].reps}` : '-';
+                const bestSet =
+                  completedSets.length > 0
+                    ? `${completedSets[0].weight} kg x ${completedSets[0].reps}`
+                    : "-";
 
                 return (
                   <TableRow key={workoutExercise.id}>
                     <TableCell>{workoutExercise.exercise.name}</TableCell>
-                    <TableCell className="text-center">{completedSets.length}</TableCell>
+                    <TableCell className="text-center">
+                      {completedSets.length}
+                    </TableCell>
                     <TableCell className="text-end">{bestSet}</TableCell>
                   </TableRow>
                 );
