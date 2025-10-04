02a2959063946c315ab6eaeecd33ac3188958083
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-10-04 08:46:35 +0200

    feat: should be able to scroll when adding exercise

--- src/app/(core)/workouts/add-exercise-button.tsx
+++ src/app/(core)/workouts/add-exercise-button.tsx
@@ -1,15 +1,25 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
-import ExercisesList from '../exercises/exercises-list';
-import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import ExercisesList from "../exercises/exercises-list";
+import { useState } from "react";
 
-export default function AddExerciseButton({ workoutId }: { workoutId: number }) {
+export default function AddExerciseButton({
+  workoutId,
+}: {
+  workoutId: number;
+}) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Drawer open={isOpen} onOpenChange={newState => setIsOpen(newState)}>
+    <Drawer open={isOpen} onOpenChange={(newState) => setIsOpen(newState)}>
       <DrawerTrigger asChild>
         <Button className="w-full">+ Add Exercise</Button>
       </DrawerTrigger>
@@ -17,8 +27,11 @@ export default function AddExerciseButton({ workoutId }: { workoutId: number }) 
         <DrawerHeader>
           <DrawerTitle>Add exercise</DrawerTitle>
         </DrawerHeader>
-        <div className="p-4">
-          <ExercisesList selectedWorkoutId={workoutId} onExerciseClick={() => setIsOpen(false)} />
+        <div className="overflow-y-auto px-4 pb-6">
+          <ExercisesList
+            selectedWorkoutId={workoutId}
+            onExerciseClick={() => setIsOpen(false)}
+          />
         </div>
       </DrawerContent>
     </Drawer>
