43969f3ec5b8ddab290bc5cdab92f3b50af0b50f
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-10-22 09:18:09 +0200

    feat: add Tabs component to ExerciseDetails

--- src/app/(core)/exercises/exercise-details/exercise-details.tsx
+++ src/app/(core)/exercises/exercise-details/exercise-details.tsx
@@ -2,6 +2,7 @@
 
 import { useExercise } from "@/api/exercises/queries";
 import ExerciseWorkoutsList from "./exercise-workouts-list";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function ExerciseDetails({
   exerciseId,
@@ -13,7 +14,19 @@ export default function ExerciseDetails({
   return (
     <div className="space-y-4">
       {data && <p className="text-center text-xl">{data.name}</p>}
-      {data && <ExerciseWorkoutsList exerciseId={data.id} />}
+
+      <Tabs defaultValue="workouts">
+        <TabsList className="w-full rounded-none">
+          <TabsTrigger value="workouts">Workouts</TabsTrigger>
+          <TabsTrigger value="charts">Charts</TabsTrigger>
+          <TabsTrigger value="records">Records</TabsTrigger>
+        </TabsList>
+        <TabsContent value="workouts">
+          {data && <ExerciseWorkoutsList exerciseId={data.id} />}
+        </TabsContent>
+        <TabsContent value="charts">Charts</TabsContent>
+        <TabsContent value="records">Records</TabsContent>
+      </Tabs>
     </div>
   );
 }
