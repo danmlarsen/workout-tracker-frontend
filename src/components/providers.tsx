0ca5c1c86ca7b543eb8dd6ce5ebd5b78c814c8b1
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-01 08:16:04 +0100

    feat: add Toaster component for notifications using Sonner library

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "@/api/auth/auth-context";
 import { queryClient } from "@/react-query/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "./ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +16,10 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           // enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <>
+            {children}
+            <Toaster position="top-center" />
+          </>
         </ThemeProvider>
       </AuthProvider>
     </QueryClientProvider>
