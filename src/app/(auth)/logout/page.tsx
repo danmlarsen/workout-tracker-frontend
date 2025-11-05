7437bae11fbd05d7e5c43cb01ae79fff548d07f9
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-05 10:49:09 +0100

    fix: prevent multiple logout requests by managing logging out state

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -2,21 +2,25 @@
 
 import { useAuth } from "@/api/auth/auth-context";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function LogoutPage() {
   const router = useRouter();
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const { logout } = useAuth();
 
   useEffect(() => {
     async function handleLogout() {
-      await logout();
-      toast.success("Successfully logged out");
-      router.push("/login");
+      if (!isLoggingOut) {
+        setIsLoggingOut(true);
+        await logout();
+        toast.success("Successfully logged out", { id: "logout-toast" });
+        router.replace("/login");
+      }
     }
     handleLogout();
-  }, [logout, router]);
+  }, [logout, router, isLoggingOut]);
 
   return null;
 }
