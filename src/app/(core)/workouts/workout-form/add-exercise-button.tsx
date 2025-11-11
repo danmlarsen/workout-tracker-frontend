bbda74625959a0edce8e69e7d3e49109e224e9eb
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-11 15:02:27 +0100

    feat: loading spinner when adding workout exercises

--- src/app/(core)/workouts/workout-form/add-exercise-button.tsx
+++ src/app/(core)/workouts/workout-form/add-exercise-button.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useAddWorkoutExercise } from "@/api/workouts/workout-exercise-mutations";
 import ExercisesView from "../../exercises/exercises-view/exercises-view";
 import { ResponsiveModal } from "@/components/ui/responsive-modal";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function AddExerciseButton({
   workoutId,
@@ -17,26 +18,36 @@ export default function AddExerciseButton({
 
   const mutateWorkoutExercise = useAddWorkoutExercise(isActiveWorkout);
 
+  function handleExerciseClick(exerciseId: number) {
+    if (mutateWorkoutExercise.isPending) return;
+
+    mutateWorkoutExercise.mutate(
+      { workoutId, exerciseId },
+      {
+        onSuccess: () => {
+          setIsOpen(false);
+        },
+      },
+    );
+  }
+
   return (
     <>
       <ResponsiveModal
         isOpen={isOpen}
         onOpenChange={setIsOpen}
         title="Add Exercise"
-        className="px-4"
         content={
-          <ExercisesView
-            onExerciseClick={(exerciseId) => {
-              mutateWorkoutExercise.mutate(
-                { workoutId, exerciseId },
-                {
-                  onSuccess: () => {
-                    setIsOpen(false);
-                  },
-                },
-              );
-            }}
-          />
+          <div className="relative">
+            <div className="px-4">
+              <ExercisesView onExerciseClick={handleExerciseClick} />
+            </div>
+            {mutateWorkoutExercise.isPending && (
+              <div className="fixed inset-0 z-50 bg-black/50">
+                <Spinner className="absolute top-1/2 left-1/2 z-50 size-6 -translate-x-1/2 -translate-y-1/2" />
+              </div>
+            )}
+          </div>
         }
       />
       <Button className="w-full" onClick={() => setIsOpen(true)}>
