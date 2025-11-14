3a9925719b7a4cf7c908503e22c1ed1a95d4039c
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-14 20:31:57 +0100

    refactor: integrate RecaptchaProvider into AuthLayout and remove from Providers

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import RecaptchaProvider from "@/components/recaptcha-provider";
+
 import Logo from "@/components/logo";
 
 interface AuthLayoutProps {
@@ -8,11 +10,13 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="mx-auto grid min-h-dvh grid-rows-[auto_1fr] px-4">
-      <div className="flex justify-center py-10">
-        <Logo />
+    <RecaptchaProvider>
+      <div className="mx-auto grid min-h-dvh grid-rows-[auto_1fr] px-4">
+        <div className="flex justify-center py-10">
+          <Logo />
+        </div>
+        <div className="grid place-items-center">{children}</div>
       </div>
-      <div className="grid place-items-center">{children}</div>
-    </div>
+    </RecaptchaProvider>
   );
 }
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,7 +6,6 @@ import { queryClient } from "@/react-query/queryClient";
 import { AuthProvider } from "@/api/auth/auth-context";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
-import RecaptchaProvider from "./recaptcha-provider";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -14,22 +13,20 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <RecaptchaProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          // enableSystem
-          // disableTransitionOnChange
-        >
-          <AuthProvider>
-            <>
-              {children}
-              <Toaster position="top-center" />
-            </>
-          </AuthProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </RecaptchaProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        // enableSystem
+        // disableTransitionOnChange
+      >
+        <AuthProvider>
+          <>
+            {children}
+            <Toaster position="top-center" />
+          </>
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
