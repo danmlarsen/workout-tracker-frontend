308d9f0ae3a214c4f8144422ff7cf194f955ded4
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-14 20:48:50 +0100

    fix: correct robots.txt rules to properly allow and disallow paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,8 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      // allow: '/',
-      disallow: "/",
+      allow: "/",
+      disallow: ["/login", "register", "/workouts", "/exercises", "/settings"],
     },
     // sitemap: 'https://acme.com/sitemap.xml',
   };
