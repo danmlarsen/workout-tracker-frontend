58a836d15fac069d18461d6866e0403946ed4fff
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-19 11:55:11 +0100

    feat: implement LandingHeader component and show respond to loggedIn state

---
+++ src/app/(public)/header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/api/auth/auth-context";
+
+export default function LandingHeader() {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <>
+      <header className="border-foreground/10 bg-card fixed inset-x-0 top-0 grid min-h-24 border-b">
+        <div className="mx-auto flex w-full max-w-5xl flex-wrap items-center justify-between gap-4 p-4 md:p-6">
+          <Logo className="h-8 w-auto -translate-y-0.5 sm:h-10" />
+          <div className="flex items-center gap-2 sm:gap-4">
+            {!isLoggedIn && (
+              <>
+                <Button asChild>
+                  <Link href="/register">Sign Up</Link>
+                </Button>
+                <Button asChild variant="outline">
+                  <Link href="/login">Log In</Link>
+                </Button>
+              </>
+            )}
+            {isLoggedIn && (
+              <>
+                <Button asChild>
+                  <Link href="/app">Go to App</Link>
+                </Button>
+                <Button asChild variant="outline">
+                  <Link href="/logout">Log Out</Link>
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </header>
+      <div className="h-24" />
+    </>
+  );
+}
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
-import Logo from "@/components/logo";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+import LandingHeader from "./header";
 
 interface LandingLayoutProps {
   children?: React.ReactNode;
@@ -9,20 +9,8 @@ interface LandingLayoutProps {
 export default function LandingLayout({ children }: LandingLayoutProps) {
   return (
     <>
-      <header className="border-foreground/10 bg-card fixed inset-x-0 top-0 grid min-h-24 border-b">
-        <div className="mx-auto flex w-full max-w-5xl items-center justify-between p-4 md:p-6">
-          <Logo className="h-8 w-auto sm:h-10" />
-          <div className="flex gap-2 sm:gap-4">
-            <Button asChild>
-              <Link href="/register">Sign Up</Link>
-            </Button>
-            <Button asChild variant="outline">
-              <Link href="/login">Log In</Link>
-            </Button>
-          </div>
-        </div>
-      </header>
-      <div className="mx-auto grid min-h-dvh max-w-5xl grid-rows-[1fr_auto] pt-24">
+      <div className="mx-auto grid min-h-dvh max-w-5xl grid-rows-[auto_1fr_auto]">
+        <LandingHeader />
         <main className="grid p-4 md:p-6">{children}</main>
         <footer className="flex items-center justify-center p-4 md:justify-end md:p-6">
           <p className="text-muted-foreground text-xs md:text-sm">
